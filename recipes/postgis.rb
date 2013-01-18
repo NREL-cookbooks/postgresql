cfbce077a0963df1faad7bbefe0944255948666e
Nick Muerdter <nick.muerdter@nrel.gov>
2013-01-17 17:13:21 -0700

    Move the custom ESRI projection into its own extension.
    This allows the activerecord-postgis-adapater to use this extension when recreating the test database.

--- recipes/postgis.rb
+++ recipes/postgis.rb
@@ -11,39 +11,15 @@ include_recipe "postgresql::server"
 
 package "postgis2_#{node[:postgresql][:version_no_dot]}"
 
-=begin
-postgresql_database "template_postgis" do
-  connection :host => "localhost", :port => node[:postgresql][:port]
-  owner "postgres"
+prefix = "/usr/pgsql-#{node[:postgresql][:version]}"
+cookbook_file "#{prefix}/share/extension/postgis_srs_esri_102003.control" do
+  mode "0644"
+  owner "root"
+  group "root"
 end
 
-postgis_init_sql = <<-EOS
-  CREATE EXTENSION IF NOT EXISTS postgis;
-  DO $$
-    BEGIN
-      IF NOT EXISTS(SELECT srid FROM spatial_ref_sys WHERE srid = 912003) THEN
-        INSERT into spatial_ref_sys (srid, auth_name, auth_srid, proj4text, srtext) values ( 999003, 'esri', 102003, '+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs ', 'PROJCS["USA_Contiguous_Albers_Equal_Area_Conic",GEOGCS["GCS_North_American_1983",DATUM["North_American_Datum_1983",SPHEROID["GRS_1980",6378137,298.257222101]],PRIMEM["Greenwich",0],UNIT["Degree",0.017453292519943295]],PROJECTION["Albers_Conic_Equal_Area"],PARAMETER["False_Easting",0],PARAMETER["False_Northing",0],PARAMETER["longitude_of_center",-96],PARAMETER["Standard_Parallel_1",29.5],PARAMETER["Standard_Parallel_2",45.5],PARAMETER["latitude_of_center",37.5],UNIT["Meter",1],AUTHORITY["EPSG","102003"]]');
-      END IF;
-    END
-  $$;
-EOS
-
-postgresql_database "template_postgis" do
-  connection :host => "localhost", :port => node[:postgresql][:port]
-  sql postgis_init_sql
-  action :query
+cookbook_file "#{prefix}/share/extension/postgis_srs_esri_102003--1.0.sql" do
+  mode "0644"
+  owner "root"
+  group "root"
 end
-
-if(node[:recipes].include?("postgresql::test_server") || node.recipe?("postgresql::test_server"))
-  postgresql_database "template_postgis" do
-    connection :host => "localhost", :port => node[:postgresql][:test][:port], :password => node[:postgresql][:test][:password][:postgres]
-    owner "postgres"
-  end
-
-  postgresql_database "template_postgis" do
-    connection :host => "localhost", :port => node[:postgresql][:test][:port], :password => node[:postgresql][:test][:password][:postgres]
-    sql postgis_init_sql
-    action :query
-  end
-end
-=end
