1555ffb6ca1cfd882225eaacb5194f6a8a37a231
Nick Muerdter <nick.muerdter@nrel.gov>
2014-03-05 23:50:28 -0700

    Another try at disabling repoforge.

--- recipes/postgis.rb
+++ recipes/postgis.rb
@@ -13,12 +13,10 @@ include_recipe "yum-epel"
 package "postgis2_#{node['postgresql']['version'].split('.').join}" do
   version node[:postgresql][:postgis_package_version]
 
-  if(node.recipe?("yum-repoforge"))
-    # Disable rpmforge while installing postgis. Without this a newer, but
-    # incompatible, version of hdf5 gets installed. We want the hdf5 library from
-    # epel.
-    options "--disablerepo=rpmforge"
-  end
+  # Disable rpmforge while installing postgis. Without this a newer, but
+  # incompatible, version of hdf5 gets installed. We want the hdf5 library from
+  # epel.
+  options "--disablerepo=rpmforge*"
 end
 
 prefix = "/usr/pgsql-#{node[:postgresql][:version]}"
