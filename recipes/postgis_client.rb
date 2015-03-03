945db28c8a815e60940df7a090c9ab55f35bdcd9
Nick Muerdter <nick.muerdter@nrel.gov>
2015-03-02 18:18:33 -0700

    Adding missing epel dependency to postgis client-only recipe.

--- recipes/postgis_client.rb
+++ recipes/postgis_client.rb
@@ -8,6 +8,7 @@
 #
 
 include_recipe "postgresql::client"
+include_recipe "yum-epel"
 
 package "postgis2_#{node['postgresql']['version'].split('.').join}" do
   version node[:postgresql][:postgis_package_version]
